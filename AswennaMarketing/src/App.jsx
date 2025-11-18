87107d6e306b6c98c66be4dfaa5a95c904c3881a
N3mxaraMe-CodeArc <nimsaraofficial@gmail.com>
2025-11-18 15:46:10 +0530

    Add initial page components for Navigation, Contact, Services, and About
    - Created Navigation component with basic structure.
    - Added Contact page component.
    - Implemented Services page component.
    - Created About page component.
    - Added Index page component with placeholder.

--- AswennaMarketing/src/App.jsx
+++ AswennaMarketing/src/App.jsx
@@ -1,10 +1,19 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css';
+import About from "./Pages/about";
+import Contact from "./Pages/Contact";
+import Index from "./Pages/index";
 
 function App() {
-
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
