7e39e44f2d1fd00194e1a93f82eb8c914e42f3ec
Nimsara Jayathilaka <127374143+Nxm-Dev@users.noreply.github.com>
2025-11-24 16:22:06 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  base: mode === 'production' ? '/Aswenna-Marketing/' : '/',
-}))
